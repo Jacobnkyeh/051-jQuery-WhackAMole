131853d404914ff45d1883bdf09b08e631008cda
Jacobnkyeh <43576836+Jacobnkyeh@users.noreply.github.com>
2019-03-05 09:22:45 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,36 @@
+$("#mole1").click(function(){
+    $("#mole1").hide();
+});
+    
+$("#moleTwo").click(function(){
+    $("#moleTwo").hide();
+}); 
+    
+$("#thirdMole").click(function(){
+    $("#thirdMole").hide();
+});
+    
+$("#thirdMole").click(function(){
+    $("#moleTwo").show();
+});
+
+$("#thirdMole").click(function(){
+    $("#mole1").show();
+});
+
+$("button").click(function(){
+    $("img").show();
+});
+
+$("#moleTwo").click(function(){
+    $("#mole1").show();
+});
+
+$("#mole1").click(function(){
+    $("#thirdMole").show();
+});
+
+
+
+
+
